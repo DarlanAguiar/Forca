c28e1d2e54966cc22d24387e1a52f014a3ca7b02
DarlanAguiar <darlangaguiar@gmail.com>
2021-11-09 10:57:13 -0400

    primeiro teste celular

--- index.js
+++ index.js
@@ -1,4 +1,64 @@
-const x = "olá, Forca"
+
+
+const palavras = ["JOGADOR", "PESSOA", "MOTOCICLETA", "RESPOSTA"]
+
+
+var array;
+function iniciar(){
+    const numero = Math.floor(Math.random()*palavras.length);
+    const palavraSelecionada = palavras[numero];
+
+    array = palavraSelecionada.split("")
+    console.log(array)
+
+    var sorteada = document.querySelector("[data-letras]");
+ 
+    for(let i = 0;i < array.length; i++){
+        
+        sorteada.innerHTML += (`<div id="${i}">_</div>`)
+        console.log(sorteada)
+
+    }
+}
+
+var numImagem = 2
+
+function confereLetra(letra){
+
+    let entrou = false
+
+    for(let i = 0; i < array.length; i++){
+        
+        
+
+        if(array[i] == letra){
+            var troca = document.getElementById(`${i}`)
+            
+            troca.innerText = `${letra}`
+            console.log(i)
+            entrou = true
+        }    
+    }
+    
+    if(!entrou){
+            
+        let foto = document.querySelector("[data-foto]");
+
+        foto.innerHTML = `<img src="img/fig${numImagem}.png" alt="">`
+
+        numImagem++
+                
+
+    }
+
+  
+
+    /* let teste = palavraSelecionada.search("A")
+
+    console.log(teste)
+ */
+    
+}
 
 
 
@@ -16,7 +76,46 @@ const x = "olá, Forca"
 
 
 
-const teste = async() => {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* const teste = async() => {
     
     const x = await fetch("https://darlanaguiar.github.io/genius/palavras.txt")
     
@@ -24,4 +123,4 @@ const teste = async() => {
     console.log(p[4])
 }
 
-teste()
+teste() */
