c719cd9588a500097b92211434ffbd012b84e6c5
DarlanAguiar <darlangaguiar@gmail.com>
2021-11-09 12:07:42 -0400

    add botão perdeu

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 
 
 const palavras = ["JOGADOR", "PESSOA", "MOTOCICLETA", "RESPOSTA"]
-
-
+var numImagem = 2
+var contador;
 var array;
 function iniciar(){
+    let perdeu = document.querySelector("[data-perdeu]")
+    perdeu.style.display = "none"
+
+    numImagem = 2
+    contador = 0
+
+    let foto = document.querySelector("[data-foto]");
+    foto.innerHTML = `<img src="img/fig1.png" alt="">`
+
+    let ganhou = document.querySelector("[data-ganhou]")
+    ganhou.style.display = "none"
+
     const numero = Math.floor(Math.random()*palavras.length);
     const palavraSelecionada = palavras[numero];
 
@@ -12,7 +24,7 @@ function iniciar(){
     console.log(array)
 
     var sorteada = document.querySelector("[data-letras]");
- 
+    sorteada.innerHTML = (``)
     for(let i = 0;i < array.length; i++){
         
         sorteada.innerHTML += (`<div id="${i}">_</div>`)
@@ -21,23 +33,36 @@ function iniciar(){
     }
 }
 
-var numImagem = 2
+function confereGanhou(numero){
+    if(numero == array.length){
+        let ganhou = document.querySelector("[data-ganhou]")
+        ganhou.style.display = "block"
+    
+    }
+}
+
+function conferePerdeu(numero){
+    if(numero == 8){
+        let perdeu = document.querySelector("[data-perdeu]")
+        perdeu.style.display = "block"
+    
+    } 
+
+}
 
 function confereLetra(letra){
 
     let entrou = false
 
     for(let i = 0; i < array.length; i++){
-        
-        
 
         if(array[i] == letra){
-            var troca = document.getElementById(`${i}`)
-            
+            var troca = document.getElementById(`${i}`)            
             troca.innerText = `${letra}`
             console.log(i)
             entrou = true
-        }    
+            contador += 1
+        }
     }
     
     if(!entrou){
@@ -47,19 +72,16 @@ function confereLetra(letra){
         foto.innerHTML = `<img src="img/fig${numImagem}.png" alt="">`
 
         numImagem++
-                
 
+        conferePerdeu(numImagem)
     }
 
-  
+    confereGanhou(contador)
 
-    /* let teste = palavraSelecionada.search("A")
 
-    console.log(teste)
- */
-    
 }
 
+iniciar()
 
 
 
